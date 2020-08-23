620df81dec8d5aa159658a9a9e1d5f373969dc93
Dennizz <root@ipaddr.nl>
2020-08-23 17:16:00 +0200

    formatting

--- output.tf
+++ output.tf
@@ -5,5 +5,5 @@ output "vcn" {
 
 output "spoke_gateway" {
   description = "The full Aviatrix OCI Spoke Gateway object"
-  value = var.ha_gw ? aviatrix_spoke_gateway.spoke_hagw[0] : aviatrix_spoke_gateway.spoke_gw[0]
+  value       = var.ha_gw ? aviatrix_spoke_gateway.spoke_hagw[0] : aviatrix_spoke_gateway.spoke_gw[0]
 }
