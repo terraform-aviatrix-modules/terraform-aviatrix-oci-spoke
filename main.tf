bd91eb89ff9b929dca30a86c351abefbbac4cf3c
Dennizz <root@ipaddr.nl>
2020-08-23 17:14:54 +0200

    cleanup

--- main.tf
+++ main.tf
@@ -1,38 +1,38 @@
 # Aviatrix OCI Spoke VPC
-resource "aviatrix_vpc" "oci_spoke_vcn" {
+resource "aviatrix_vpc" "default" {
   cloud_type   = 16
-  name         = "av-${var.oci_region}-spoke-vcn"
-  region       = var.oci_region
-  cidr         = var.oci_spoke_cidr
-  account_name = var.oci_account_name
+  name         = "avx-${var.name}-spoke"
+  region       = var.region
+  cidr         = var.cidr
+  account_name = var.account
 }
 
 # Single OCI Spoke Gateway 
-resource "aviatrix_spoke_gateway" "oci_spoke_gw" {
+resource "aviatrix_spoke_gateway" "spoke_gw" {
   count              = var.ha_gw ? 0 : 1
-  gw_name            = "${var.oci_region}-spoke"
-  vpc_id             = aviatrix_vpc.oci_spoke_vcn.name
+  gw_name            = "avx-${var.name}-spoke"
+  vpc_id             = aviatrix_vpc.default.name
   cloud_type         = 16
-  vpc_reg            = var.oci_region
+  vpc_reg            = var.region
   enable_active_mesh = true
-  gw_size            = var.oci_gw_size
-  account_name       = var.oci_account_name
-  subnet             = aviatrix_vpc.oci_spoke_vcn.subnets[0].cidr
+  gw_size            = var.instance_size
+  account_name       = var.account
+  subnet             = aviatrix_vpc.default.subnets[0].cidr
   transit_gw         = var.transit_gw
 }
 
 # HA OCI Spoke Gateway 
-resource "aviatrix_spoke_gateway" "oci_spoke_hagw" {
+resource "aviatrix_spoke_gateway" "spoke_hagw" {
   count              = var.ha_gw ? 1 : 0
-  gw_name            = "${var.oci_region}-spoke"
-  vpc_id             = aviatrix_vpc.oci_spoke_vcn.name
+  gw_name            = "avx-${var.name}-spoke"
+  vpc_id             = aviatrix_vpc.default.name
   cloud_type         = 16
-  vpc_reg            = var.oci_region
+  vpc_reg            = var.region
   enable_active_mesh = true
-  gw_size            = var.oci_gw_size
-  account_name       = var.oci_account_name
-  subnet             = aviatrix_vpc.oci_spoke_vcn.subnets[0].cidr
-  ha_subnet          = aviatrix_vpc.oci_spoke_vcn.subnets[0].cidr
-  ha_gw_size         = var.oci_gw_size
+  gw_size            = var.instance_size
+  account_name       = var.account
+  subnet             = aviatrix_vpc.default.subnets[0].cidr
+  ha_subnet          = aviatrix_vpc.default.subnets[0].cidr
+  ha_gw_size         = var.instance_size
   transit_gw         = var.transit_gw
 }
